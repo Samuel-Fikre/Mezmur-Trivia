350cd592044ef436c5a5c35255e2d8bda2211d9b
Samuel Fikre <samicreed12@gmail.com>
2024-11-30 12:18:39 +0300

    analytics added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import { Toaster } from "$lib/components/ui/sonner";
+	import { injectAnalytics } from '@vercel/analytics/sveltekit'
 	let { children } = $props();
 </script>
 
